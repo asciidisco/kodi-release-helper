2390580c3b3d86499fe4aab8f00552523e3849da
asciidisco <public@asciidisco.com>
2017-10-04 09:49:06 +0200

    fix(core): Check for empty entries & wrong scopes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kodi-release-helper',
-    version='1.0.1',
+    version='1.0.2',
     description='Kodi Release Helper',
     author='asciidisco',
     author_email='public@asciidisco.com',
